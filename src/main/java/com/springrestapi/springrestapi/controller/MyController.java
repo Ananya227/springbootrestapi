396e554717883cec9cde35957973a87594306961
Ananya227 <ananyayadav227@gmail.com>
2024-07-08 18:48:42 +0530

    Initial commit

---
+++ src/main/java/com/springrestapi/springrestapi/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.springrestapi.springrestapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController
+
+{
+
+      public  String home()
+      {
+
+          return " Welcomt to new home";
+
+      }
+
+
+}
